0cd3dc188b241989ab3c7f84b90a6d9dc8ef8634
redstoneleo <redstone-cold@163.com>
2020-12-01 22:04:19 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -7,7 +7,20 @@
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
-(function() {
-    'use strict';
-    
-})();
+(
+  function(){
+      var styleSheet = document.createElement("style"); 
+      styleSheet.type = "text/css"; 
+      styleSheet.innerText =`.hypothesis-highlight {background-color: rgba(255, 150, 255, 0.5) !important;}`; 
+      document.head.appendChild(styleSheet);
+      
+      
+    window.hypothesisConfig=function(){
+      return{showHighlights:true,appType:'bookmarklet'};
+    };
+    var d=document;
+    var s=d.createElement('script');
+    s.setAttribute('src','https://hypothes.is/embed.js');
+    d.body.appendChild(s);
+  }
+)();
